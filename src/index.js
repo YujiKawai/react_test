33e6766be95f32e6bda4c6c6c9b172c731b0393e
Yuji Kawai <kawai@odk.co.jp>
2022-03-28 22:29:38 +0900

    20210328

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import "./styles.css";
+/**
+ * const, let 変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数再宣言可能
+// var val1 = "var変数再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// let val2 = "let再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // val3 = "const上書き";
+
+// const val3 = "const再宣言";
+
+// const val4 = {
+//   name: "ふが",
+//   age: "28"
+// }
+// val4.name = "fuga";
+// val4.address = "osaka";
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
